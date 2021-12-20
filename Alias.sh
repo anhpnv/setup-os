e65f4ae6b9bdbcb629f7408fb95291fda60343fd
vietanhphamnhu <40441000+vietanhphamnhu@users.noreply.github.com>
2021-12-20 21:19:58 +0700

    Update right code

--- Alias.sh
+++ Alias.sh
@@ -1,3 +1,3 @@
-echo alias dockcomdown='docker-compose down -v' >> ~/.bashrc
-echo alias dockcomup='docker-compose up -d' >> ~/.bashrc
-echo alias dockcomlog='docker-compose logs -f' >> ~/.bashrc
+echo "alias dockcomdown='docker-compose down -v'" >> ~/.bashrc
+echo "alias dockcomup='docker-compose up -d'" >> ~/.bashrc
+echo "alias dockcomlog='docker-compose logs -f'" >> ~/.bashrc
